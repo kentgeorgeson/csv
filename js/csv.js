a1e6cb96f8a8bc76d86ee8449e723dc13114d492
Kent Georgeson <kent@georgesons.net>
2013-11-25 16:56:45 -0500

    Latest changes

--- js/csv.js
+++ js/csv.js
@@ -30,7 +30,6 @@
     // read the file metadata
     var output = ''
         output += '<span style="font-weight:bold;">File Statistics</span><br />\n';
-        output += ' - FileType: ' + (file.type || 'n/a') + '<br />\n';
         output += ' - FileSize: ' + file.size + ' bytes<br />\n';
         output += ' - LastModified: ' + (file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a') + '<br />\n';
 
@@ -79,14 +78,101 @@
 	    } 
       }
 	  html += '</tbody>\r\n';
-      
-	
+    
 	  $('#dataTable').html(html);
 
-	  $('#dataTable').dataTable().columnFilter();
-
-		$('#dataTable').dataTable( {
-			"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+	  $('#dataTable').dataTable({
+			"bPaginate": true,
+			"sPaginationType": "full_numbers",
+			"bfilter": true,
+			"bSort": true,
+			"bFilter": true,
+			"bDestroy": true
+			
+		});
+		
+		var oTable = $('#dataTable').dataTable( {
+			"oLanguage": {
+						"sSearch": "Search all columns:"
+			}
+		});
+		
+		$("thead tr.autofilter th").each( function ( i ) {
+				this.innerHTML = fnCreateSelect( oTable.fnGetColumnData(i) );
+				$('select', this).change( function () {
+					oTable.fnFilter( $(this).val(), i );
+				} );
+			} );
     };
     reader.onerror = function(){ alert('Unable to read ' + file.fileName); };
   }
+
+
+/**********************
+* Overrides for Select Filter
+**********************/
+
+(function($) {
+/*
+ * Function: fnGetColumnData
+ * Purpose:  Return an array of table values from a particular column.
+ * Returns:  array string: 1d data array 
+ * Inputs:   object:oSettings - dataTable settings object. This is always the last argument past to the function
+ *           int:iColumn - the id of the column to extract the data from
+ *           bool:bUnique - optional - if set to false duplicated values are not filtered out
+ *           bool:bFiltered - optional - if set to false all the table data is used (not only the filtered)
+ *           bool:bIgnoreEmpty - optional - if set to false empty values are not filtered from the result array
+ * Author:   Benedikt Forchhammer <b.forchhammer /AT\ mind2.de>
+ */
+$.fn.dataTableExt.oApi.fnGetColumnData = function ( oSettings, iColumn, bUnique, bFiltered, bIgnoreEmpty ) {
+	// check that we have a column id
+	if ( typeof iColumn == "undefined" ) return new Array();
+	
+	// by default we only wany unique data
+	if ( typeof bUnique == "undefined" ) bUnique = true;
+	
+	// by default we do want to only look at filtered data
+	if ( typeof bFiltered == "undefined" ) bFiltered = true;
+	
+	// by default we do not wany to include empty values
+	if ( typeof bIgnoreEmpty == "undefined" ) bIgnoreEmpty = true;
+	
+	// list of rows which we're going to loop through
+	var aiRows;
+	
+	// use only filtered rows
+	if (bFiltered == true) aiRows = oSettings.aiDisplay; 
+	// use all rows
+	else aiRows = oSettings.aiDisplayMaster; // all row numbers
+
+	// set up data array	
+	var asResultData = new Array();
+	
+	for (var i=0,c=aiRows.length; i<c; i++) {
+		iRow = aiRows[i];
+		var aData = this.fnGetData(iRow);
+		var sValue = aData[iColumn];
+		
+		// ignore empty values?
+		if (bIgnoreEmpty == true && sValue.length == 0) continue;
+
+		// ignore unique values?
+		else if (bUnique == true && jQuery.inArray(sValue, asResultData) > -1) continue;
+		
+		// else push the value onto the result data array
+		else asResultData.push(sValue);
+	}
+	
+	return asResultData;
+}}(jQuery));
+
+
+function fnCreateSelect( aData )
+{
+	var r='<select><option value=""></option>', i, iLen=aData.length;
+	for ( i=0 ; i<iLen ; i++ )
+	{
+		r += '<option value="'+aData[i]+'">'+aData[i]+'</option>';
+	}
+	return r+'</select>';
+}
